8fe67930150347e4892af8455df4e125cc11e193
Richard Leivers <98045240+RichardGenH@users.noreply.github.com>
2023-01-27 16:14:01 +0000

    adding tailwind elements accordion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://RichardGenH.github.io',
   base: '/astro-criteria',
-})
+  integrations: [react(), tailwind()]
+});
